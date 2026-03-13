97020b9e7841c98ee4080910af86962066c8f01c
P Surender Reddy <surender.scorpion@gmail.com>
2026-03-13 11:49:52 +0530

    mock AI responses for demo

--- src/server.js
+++ src/server.js
@@ -6,11 +6,14 @@ import proposalRoutes from "./routes/proposal.js"
 dotenv.config()
 const app = express()
 app.use(express.json())
+app.get("/", (req, res) => {
+  res.send("Rayeva AI Assignment API is running 🚀");
+});
 
 app.use("/api/catalog", catalogRoutes)
 app.use("/api/proposal", proposalRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`)
+  console.log(`http://localhost:${PORT}`)
 })
